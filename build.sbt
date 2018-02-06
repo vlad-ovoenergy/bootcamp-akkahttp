af5e2b5264c184ab937c0e63b20edcb60070dd98
vladimirsivanovs <vladimirs.ivanovs@ovoenergy.com>
2018-02-06 15:31:52 +0000

    initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "bootcamp-akkahttp"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http" % "10.0.11",
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.0.11" % Test
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,34 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest}
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.{Await, Promise}
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
+
+
+object Main extends App {
+
+  implicit val system = ActorSystem("vlad")
+  implicit val ec = system.dispatcher
+  implicit val mat = ActorMaterializer()
+
+  val site = Http().singleRequest(HttpRequest(HttpMethods.GET, "https://stackoverflow.com/questions/32315789/akka-httpresponse-read-body-as-string-scala"))
+
+
+  site onComplete {
+    case Success(html) => {
+      html.entity.toStrict(20 seconds)
+        .map(_.data)
+        .map(_.utf8String)
+        .map(s => s.split(" ").filter(_.contains("href=\"https:")))
+        .filter(!_.isEmpty)
+        .map(x => x.mkString("\n"))
+        .map( x => println(x))
+    }
+    case Failure(f) => println(f)
+  }
+
+
+}
