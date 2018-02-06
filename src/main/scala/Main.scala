d6c935c3901ba0214e7173e56981da17828eb956
vladimirsivanovs <vladimirs.ivanovs@ovoenergy.com>
2018-02-06 15:36:46 +0000

    Using unmarshaller

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,6 +7,7 @@ import scala.concurrent.{Await, Promise}
 import scala.util.{Failure, Success}
 import scala.concurrent.duration._
 
+import akka.http.scaladsl.unmarshalling.Unmarshal
 
 object Main extends App {
 
@@ -17,11 +18,23 @@ object Main extends App {
   val site = Http().singleRequest(HttpRequest(HttpMethods.GET, "https://stackoverflow.com/questions/32315789/akka-httpresponse-read-body-as-string-scala"))
 
 
+//  site onComplete {
+//    case Success(html) => {
+//      html.entity.toStrict(20 seconds)
+//        .map(_.data)
+//        .map(_.utf8String)
+//        .map(s => s.split(" ").filter(_.contains("href=\"https:")))
+//        .filter(!_.isEmpty)
+//        .map(x => x.mkString("\n"))
+//        .map( x => println(x))
+//    }
+//    case Failure(f) => println(f)
+//  }
+
+
   site onComplete {
     case Success(html) => {
-      html.entity.toStrict(20 seconds)
-        .map(_.data)
-        .map(_.utf8String)
+      Unmarshal(html.entity).to[String]
         .map(s => s.split(" ").filter(_.contains("href=\"https:")))
         .filter(!_.isEmpty)
         .map(x => x.mkString("\n"))
@@ -29,6 +42,7 @@ object Main extends App {
     }
     case Failure(f) => println(f)
   }
+
 
 
 }
